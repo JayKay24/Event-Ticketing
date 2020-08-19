96b9a601361106001d21940859ad7dc0a042ea50
James Kinyua <jameskinyua590@gmail.com>
2020-08-19 21:06:12 +0300

    create a user model definition using Typescript

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
+import mongoose from "mongoose";
 
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
@@ -25,6 +26,21 @@ app.all("*", (req, res) => {
 
 app.use(errorHandler);
 
-app.listen(port, () => {
-  console.log(`App is listening on port ${port}!!!!!`);
-});
+const start = async () => {
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log("Connected to mongoDB");
+  } catch (err) {
+    console.error(err);
+  }
+
+  app.listen(port, () => {
+    console.log(`App is listening on port ${port}!!!!!`);
+  });
+};
+
+start();
