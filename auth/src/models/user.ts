3f34c0d09f2ec97bcdd387bf2cc228e700368bf5
James Kinyua <jameskinyua590@gmail.com>
2020-08-20 23:34:15 +0300

    handle bad requests and handle password hashing

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { Password } from "../services/password";
 
 // An interface that describes the properties
 // that are required to create a new user
@@ -8,8 +9,15 @@ interface userAttrs {
 }
 
 // An interface that describes the properties that the User Model has
-interface UserModel extends mongoose.Model<any> {
-  build(attrs: userAttrs): any;
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: userAttrs): UserDoc;
+}
+
+// An interface that describes the properties that a
+// User document has
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
 }
 
 const userSchema = new mongoose.Schema({
@@ -23,8 +31,18 @@ const userSchema = new mongoose.Schema({
   },
 });
 
+userSchema.pre("save", async function (done) {
+  if (this.isModified("password")) {
+    const hashed = await Password.toHash(this.get("password"));
+    this.set("password", hashed);
+  }
+  done();
+});
+
 userSchema.statics.build = (attrs: userAttrs) => new User(attrs);
 
-const User = mongoose.model<any, UserModel>("User", userSchema);
+const User = mongoose.model<UserDoc, UserModel>("User", userSchema);
+
+const user = User.build({ email: "", password: "" });
 
 export { User };
