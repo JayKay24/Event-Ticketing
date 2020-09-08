27901715e3fd83a659b825aa4cce8452a7aaa60c
James Kinyua <jameskinyua590@gmail.com>
2020-09-08 20:30:41 +0300

    include extra parameter on useRequest for success callbacks

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import axios from "axios";
+import Router from "next/router";
+
 import useRequest from "../../hooks/useRequest";
 
 export default () => {
@@ -9,12 +10,13 @@ export default () => {
     url: "/api/users/signup",
     method: "post",
     body: { email, password },
+    onSuccess: () => Router.push("/"),
   });
 
   const onSubmit = async (event) => {
     event.preventDefault();
 
-    doRequest();
+    await doRequest();
   };
 
   return (
