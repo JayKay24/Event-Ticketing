e51512c64c7949b2f12e50423e5f2b54e34eaafb
James Kinyua <jameskinyua590@gmail.com>
2020-09-28 21:11:46 +0300

    implement initial structure of updateTicketRouter

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -8,6 +8,7 @@ import { errorHandler, NotFoundError, currentUser } from "@jkntickets/common";
 import { createTicketRouter } from "./routes/new";
 import { showTicketRouter } from "./routes/show";
 import { indexTicketRouter } from "./routes/index";
+import { updateTicketRouter } from "./routes/update";
 
 const app = express();
 
@@ -26,6 +27,7 @@ app.use(currentUser);
 app.use(createTicketRouter);
 app.use(showTicketRouter);
 app.use(indexTicketRouter);
+app.use(updateTicketRouter);
 
 app.all("*", (req, res) => {
   throw new NotFoundError();
