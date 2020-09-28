137b87897810a853507d277005aff69cd412039a
James Kinyua <jameskinyua590@gmail.com>
2020-09-28 20:30:14 +0300

    generate a valid id for show.test.ts

--- tickets/src/routes/__test__/show.test.ts
+++ tickets/src/routes/__test__/show.test.ts
@@ -1,8 +1,10 @@
 import request from "supertest";
+import mongoose from "mongoose";
 import { app } from "../../app";
 
 it("returns a 404 if a ticket is not found", async () => {
-  await request(app).get("/api/tickets/sferferfe").send().expect(404);
+  const id = new mongoose.Types.ObjectId();
+  await request(app).get(`/api/tickets/${id}`).send().expect(404);
 });
 
 it("returns the ticket if a ticket is found", async () => {
