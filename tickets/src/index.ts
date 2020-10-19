2ce272fd97067251a78fc568f86fa380796c6df9
James Njuguna <jameskinyua590@gmail.com>
2020-10-19 20:48:16 +0300

    create a Stan singleton to be shared around the tickets service

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { natsWrapper } from "./nats-wrapper";
 
 import { app } from "./app";
 
@@ -12,6 +13,7 @@ const start = async () => {
     throw new Error("MONGO_URI must be defined");
   }
   try {
+    await natsWrapper.connect("ticketing", "refre", "http://nats-srv:4222");
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -3,6 +3,7 @@ import { body } from "express-validator";
 import { requireAuth, validateRequest } from "@jkntickets/common";
 
 import { Ticket } from "../models/ticket";
+import { TicketCreatedPublisher } from "../events/publishers/ticket-created-publisher";
 
 const router = express.Router();
 
@@ -22,6 +23,13 @@ router.post(
     const ticket = Ticket.build({ title, price, userId: req.currentUser!.id });
 
     await ticket.save();
+
+    // new TicketCreatedPublisher(client).publish({
+    //   id: ticket.id,
+    //   title: ticket.title,
+    //   price: ticket.price,
+    //   userId: ticket.userId,
+    // });
 
     res.status(201).send(ticket);
   }
