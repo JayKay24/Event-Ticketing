9814a801d2c4365aa7338286511d346e7938e669
James Njuguna <jameskinyua590@gmail.com>
2020-10-22 21:03:09 +0300

    setup skaffold for the orders service

--- copy_common_files_from_auth.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-#: Description: copy common files from the auth service to hasten development
-
-cp $( pwd )/../auth/{.dockerignore,Dockerfile,package.json,package-lock.json,tsconfig.json} .
-
-mkdir src && cd $_
-
-cp -R $( pwd )/../auth/src/{test,app.ts,index.ts} .
-
---
+++ copy_common_files_from_tickets.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+#: Description: copy common files from the tickets service to hasten development
+#: . ./copy_common_files_from_tickets.sh $1
+
+# echo parameters here "0: $0" "1: $1" "2: $2"
+
+service_name=tickets
+new_service=$1
+
+mkdir $new_service && cd $_
+
+service_path=${HOME}/Desktop/personal_learning/microservices/ticketing/${service_name}
+
+cp ${service_path}/{.dockerignore,Dockerfile,package.json,package-lock.json,tsconfig.json} .
+
+mkdir src && cd $_
+
+cp -R ${service_path}/src/{test,app.ts,index.ts,nats-wrapper.ts} .
+
+cd ..
+
+cp ../infra/k8s/${service_name}-depl.yaml ../infra/k8s/${new_service}-depl.yaml
+cp ../infra/k8s/${service_name}-mongo-depl.yaml ../infra/k8s/${new_service}-mongo-depl.yaml
+
+sed -i -e "s/'${service_name}'/'${new_service}'/g"
+
+npm i && docker build -t jaykay24/$service_name
