44de3e2ce6a16d0cfadbc88e293bfee45c6dc4d3
James Njuguna <jameskinyua590@gmail.com>
2020-10-26 21:27:05 +0300

    implement OrderStatus enum

--- orders/src/models/order.ts
+++ orders/src/models/order.ts
@@ -1,15 +1,16 @@
-import mongoose, { mongo } from "mongoose";
+import mongoose from "mongoose";
+import { OrderStatus } from "@jkntickets/common";
 
 interface OrderAttrs {
   userId: string;
-  status: string;
+  status: OrderStatus;
   expiresAt: Date;
   ticket: TicketDoc;
 }
 
 interface OrderDoc extends mongoose.Document {
   userId: string;
-  status: string;
+  status: OrderStatus;
   expiresAt: Date;
   ticket: TicketDoc;
 }
@@ -21,7 +22,12 @@ interface OrderModel extends mongoose.Model<OrderDoc> {
 const orderSchema = new mongoose.Schema(
   {
     userId: { type: String, required: true },
-    status: { type: String, required: true },
+    status: {
+      type: String,
+      required: true,
+      enum: Object.values(OrderStatus),
+      default: OrderStatus.Created,
+    },
     expiresAt: { type: mongoose.Schema.Types.Date },
     ticket: { type: mongoose.Schema.Types.ObjectId, ref: "Ticket" },
   },
