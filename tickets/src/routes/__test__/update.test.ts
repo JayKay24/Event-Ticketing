0446057d1df74f4050f1124972e5fe31e51bba15
James Njuguna <jameskinyua590@gmail.com>
2021-02-17 23:28:12 +0300

    test it rejects updates to a ticket if the ticket is reserved in tickets service

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -1,6 +1,8 @@
 import request from "supertest";
+import mongoose, { mongo } from "mongoose";
+
 import { app } from "../../app";
-import mongoose from "mongoose";
+import { Ticket } from "../../models/ticket";
 import { natsWrapper } from "../../nats-wrapper";
 
 it("returns a 404 if the provided id does not exist", async () => {
@@ -103,4 +105,26 @@ it("publishes an event", async () => {
     .expect(200);
 
   expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
+
+it("rejects updates if the ticket is reserved", async () => {
+  const cookie = global.signin();
+
+  const response = await request(app)
+    .post("/api/tickets")
+    .set("Cookie", cookie)
+    .send({ title: "adswed", price: 20 });
+
+  const ticket = await Ticket.findById(response.body.id);
+  ticket!.set({ orderId: mongoose.Types.ObjectId().toHexString() });
+  await ticket!.save();
+
+  const title = "new title",
+    price = 40;
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set("Cookie", cookie)
+    .send({ title, price })
+    .expect(400);
 });
