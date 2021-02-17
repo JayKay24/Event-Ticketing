7e5fd02f75ad460422820b38a2331ba02a730765
James Njuguna <jameskinyua590@gmail.com>
2021-02-17 21:23:39 +0300

    publish new ticket version in the tickets service when an order is created

--- tickets/src/events/listeners/order-created-listener.ts
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -3,6 +3,7 @@ import { Listener, OrderCreatedEvent, Subjects } from "@jkntickets/common";
 
 import { queueGroupName } from "./queue-group-name";
 import { Ticket } from "../../models/ticket";
+import { TicketUpdatedPublisher } from "../publishers/ticket-updated-publisher";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated;
@@ -23,6 +24,14 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
 
     // Save the ticket
     await ticket.save();
+    await new TicketUpdatedPublisher(this.client).publish({
+      id: ticket.id,
+      version: ticket.version,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+      orderId: ticket.orderId,
+    });
 
     // ack the message
     msg.ack();
