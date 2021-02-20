26b43dca6e5a750d5c66e562f846996e5b4ace81
James Njuguna <jameskinyua590@gmail.com>
2021-02-20 22:33:24 +0300

    wire up expirationQueue to order-created-listener in expiration service

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -2,10 +2,15 @@ import { Message } from "node-nats-streaming";
 import { Listener, OrderCreatedEvent, Subjects } from "@jkntickets/common";
 
 import { queueGroupName } from "./queue-group-name";
+import { expirationQueue } from "../../queues/expiration-queue";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated;
   queueGroupName = queueGroupName;
 
-  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {}
+  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
+    await expirationQueue.add({ orderId: data.id });
+
+    msg.ack();
+  }
 }
--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -1,6 +1,5 @@
 import { natsWrapper } from "./nats-wrapper";
-
-const port = 3000;
+import { OrderCreatedListener } from "./events/listeners/order-created-listener";
 
 const start = async () => {
   if (!process.env.NATS_CLIENT_ID) {
@@ -28,6 +27,8 @@ const start = async () => {
 
     process.on("SIGINT", () => natsWrapper.client.close());
     process.on("SIGTERM", () => natsWrapper.client.close());
+
+    new OrderCreatedListener(natsWrapper.client).listen();
   } catch (err) {
     console.error(err);
   }
