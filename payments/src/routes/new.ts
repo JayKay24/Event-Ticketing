480280528297d5ce2d856543fca1af23b8f78f0b
James Njuguna <jameskinyua590@gmail.com>
2021-02-22 10:05:57 +0300

    configure stripe sdk in payments service

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -9,6 +9,7 @@ import {
   OrderStatus,
 } from "@jkntickets/common";
 
+import { stripe } from "../stripe";
 import { Order } from "../models/order";
 
 const router = express.Router();
@@ -34,6 +35,12 @@ router.post(
     if (order.status === OrderStatus.Cancelled) {
       throw new BadRequestError("Cannot pay for a cancelled order");
     }
+
+    await stripe.charges.create({
+      currency: "usd",
+      amount: order.price * 100,
+      source: token,
+    });
 
     res.send({ success: true });
   }
